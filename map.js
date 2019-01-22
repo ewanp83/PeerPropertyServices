59b8748ed1b235ad28e1f5ff31966f85511520ba
Ewan Peer <ewanpeer@gmail.com>
2019-01-22 13:55:23 +0000

    added extra pics and layout for testimonials. Page sizings modified generally

--- map.js
+++ map.js
@@ -5,11 +5,11 @@ function initMap() {
     });
     
     var coverageCircle = new google.maps.Circle({
-      strokeColor: '#FF0000',
+      strokeColor: '#0e2f63',
       strokeOpacity: 0.8,
       strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35,
+      fillColor: '#0e2f63',
+      fillOpacity: 0.2,
       map: map,
       center: {lat: 52.420782, lng: -1.763291},
       radius: 8000
