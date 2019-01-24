91ea08595944950e5ff72a942695b181974e0031
Ewan Peer <ewanpeer@gmail.com>
2019-01-24 11:21:20 +0000

    solved link problem for picture and map, edited hire us button

--- map.js
+++ map.js
@@ -9,9 +9,10 @@ function initMap() {
       strokeOpacity: 0.8,
       strokeWeight: 2,
       fillColor: '#0e2f63',
-      fillOpacity: 0.2,
+      fillOpacity: 0.05,
       map: map,
       center: {lat: 52.420782, lng: -1.763291},
       radius: 8000
     });
 }
+
